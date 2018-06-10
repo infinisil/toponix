076c8321e9d521a01ce0787f758a7f23f3f0444d
Silvan Mosberger <infinisil@icloud.com>
2018-06-10 22:19:26 +0200

    Make config configurable (duh)

---
+++ exampleConfig.nix
@@ -0,0 +1,36 @@
+{ config, ... }: {
+
+  topology = {
+    home = {
+      pc = "192.168.1.25";
+      laptop = "192.168.1.19";
+    };
+    otherserver = "164.165.203.40";
+    server = "207.154.198.134";
+    public = {
+      laptop = null;
+    };
+  };
+
+  rssh = {
+    enable = true;
+    ipPorts = n: toString (6726 + n);
+    server = config.hosts.server;
+    clients = with config.hosts; [
+      pc
+      laptop
+    ];
+  };
+
+  openvpn = {
+    enable = true;
+    addresses = n: "10.176.75.${toString n}";
+    server = config.hosts.server;
+    clients = with config.hosts; [
+      pc
+      laptop
+      otherserver
+    ];
+  };
+
+}
